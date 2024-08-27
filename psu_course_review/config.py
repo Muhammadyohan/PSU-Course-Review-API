9b709ba9d2f933f09360c5730256e4736a40e348
Muhammadyohan Kachey <yohun2002@gmail.com>
2024-08-27 21:39:05 +0700

    modify files and delete testing.env file

--- psu_course_review/config.py
+++ psu_course_review/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     SQLDB_URL: str
-    SECRET_KEY: str = "secret"
+    SECRET_KEY: str
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 5 * 60  # 5 minutes
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 7 * 24 * 60  # 7 days
