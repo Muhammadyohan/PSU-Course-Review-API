e7d63cf1c46caa5edca02377f9e606b26d57e8a9
Muhammadyohan Kachey <yohun2002@gmail.com>
2024-10-14 23:49:40 +0700

    add cors middleware

--- psu_course_review/main.py
+++ psu_course_review/main.py
@@ -1,8 +1,5 @@
-from gevent import monkey
-
-monkey.patch_all()
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from . import config
@@ -11,23 +8,27 @@ from . import models
 
 from . import routers
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Create tables during startup
+    # Startup
     await models.create_table()
     yield
-    if models.engine is not None:
-        # Close the DB connection
-        await models.close_session()
 
 
 def create_app(settings=None):
     if not settings:
         settings = config.get_settings()
-
     app = FastAPI(lifespan=lifespan)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     models.init_db(settings)
     routers.init_router(app)
+
     return app
